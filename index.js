da0fae86e6b62b89b288aeef227ffc61d60807e8
Bernard <bernard.koech@student.moringaschool.com>
2022-08-19 23:16:25 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const removeMain = document.getElementById('main');
+removeMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Bernard is the champion';
